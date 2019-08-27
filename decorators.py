391283d9a9421c50498661880da4f4030c6c6a72
Oscar Tematio <omtematio@gmail.com>
2019-08-27 10:00:43 +0200

    initial commit

---
+++ decorators.py
@@ -0,0 +1,12 @@
+from functools import wraps
+from flask import session, redirect, url_for
+
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            return redirect(url_for('login'))
+
+    return wrap
